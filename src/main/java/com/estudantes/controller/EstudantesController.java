0a2a2a4ddeed6e977a1f89e3e9121adb1d090e7f
gabriel guariblia <gabrielguariglia@gmail.com>
2021-06-22 19:06:47 -0300

    colocando crossorigin

--- src/main/java/com/estudantes/controller/EstudantesController.java
+++ src/main/java/com/estudantes/controller/EstudantesController.java
@@ -9,6 +9,7 @@ import com.estudantes.repository.EstudantesRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/estudantes")
+@CrossOrigin
 
 public class EstudantesController {
 
